bd389beb9efda3c6ded2c4dbe2e25ce70b6bf615
Santo Cariotti <dcariotti24@gmail.com>
2020-06-09 21:38:28 +0200

    fix: registers closed in a single array

--- src/processor/registers.rs
+++ src/processor/registers.rs
@@ -17,18 +17,7 @@ impl CSPR {
 }
 
 pub struct Registers {
-    pub r1: i32,
-    r2: i32,
-    r3: i32,
-    r4: i32,
-    r5: i32,
-    r6: i32,
-    r7: i32,
-    r8: i32,
-    r9: i32,
-    r10: i32,
-    r11: i32,
-    r12: i32,
+    pub r : [i32; 12],
     ip: i32,
     sp: i32,
     lr: i32,
@@ -39,18 +28,7 @@ pub struct Registers {
 impl Registers {
     pub fn new() -> Self {
         Registers {
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            r6: 0,
-            r7: 0,
-            r8: 0,
-            r9: 0,
-            r10: 0,
-            r11: 0,
-            r12: 0,
+            r: [0; 12],
             ip: 0,
             sp: 0,
             lr: 0,
